4f8032b7d5db8a539ae864fcc36e22048f882751
BDang029 <21110764@student.hcmute.edu.vn>
2023-11-15 14:29:40 +0700

    okeeee

--- src/main/java/murach/business/Cart.java
+++ src/main/java/murach/business/Cart.java
@@ -19,27 +19,7 @@ public class Cart implements Serializable {
         return items.size();
     }
 
-    package murach.business;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-
-public class Cart implements Serializable {
-
-    private ArrayList<LineItem> items;
-
-    public Cart() {
-        items = new ArrayList<LineItem>();
-    }
-
-    public ArrayList<LineItem> getItems() {
-        return items;
-    }
-
-    public int getCount() {
-        return items.size();
-    }
-     public void addItem(LineItem item) {
+    public void addItem(LineItem item) {
         String code = item.getProduct().getCode();
         int quantity = item.getQuantity();
         for (int i = 0; i < items.size(); i++) {
@@ -56,19 +36,6 @@ public class Cart implements Serializable {
         }
         items.add(item);
     }
-
-
-    public void removeItem(LineItem item) {
-        String code = item.getProduct().getCode();
-        for (int i = 0; i < items.size(); i++) {
-            LineItem lineItem = items.get(i);
-            if (lineItem.getProduct().getCode().equals(code)) {
-                items.remove(i);
-                return;
-            }
-        }
-    }
-}
 
     public void removeItem(LineItem item) {
         String code = item.getProduct().getCode();
