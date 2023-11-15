7c310693dbfc15e58895a71c2fcf02eaa442f314
BDang029 <21110764@student.hcmute.edu.vn>
2023-11-15 14:25:24 +0700

    additem

--- src/main/java/murach/business/Cart.java
+++ src/main/java/murach/business/Cart.java
@@ -19,18 +19,56 @@ public class Cart implements Serializable {
         return items.size();
     }
 
+    package murach.business;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Cart implements Serializable {
+
+    private ArrayList<LineItem> items;
+
+    public Cart() {
+        items = new ArrayList<LineItem>();
+    }
+
+    public ArrayList<LineItem> getItems() {
+        return items;
+    }
+
+    public int getCount() {
+        return items.size();
+    }
+
     public void addItem(LineItem item) {
         String code = item.getProduct().getCode();
         int quantity = item.getQuantity();
         for (int i = 0; i < items.size(); i++) {
             LineItem lineItem = items.get(i);
             if (lineItem.getProduct().getCode().equals(code)) {
-                lineItem.setQuantity(quantity);
+                if (lineItem.getQuantity() != 1 && quantity != lineItem.getQuantity()) {
+                    lineItem.setQuantity(lineItem.getQuantity() + quantity);
+                }
+                else {
+                    lineItem.setQuantity(quantity);
+                }
                 return;
             }
         }
         items.add(item);
     }
+
+    public void removeItem(LineItem item) {
+        String code = item.getProduct().getCode();
+        for (int i = 0; i < items.size(); i++) {
+            LineItem lineItem = items.get(i);
+            if (lineItem.getProduct().getCode().equals(code)) {
+                items.remove(i);
+                return;
+            }
+        }
+    }
+}
 
     public void removeItem(LineItem item) {
         String code = item.getProduct().getCode();
